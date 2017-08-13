330b05557b627b642938a3cd6d63504b903e5d46
SAM JUNIOR <younggeeks101@gmail.com>
2017-08-13 19:26:18 +0300

    first commit

---
+++ Social-fire/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social-fire
+//
+//  Created by Samwel Charles on 13/08/2017.
+//  Copyright © 2017 younggeeks. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
